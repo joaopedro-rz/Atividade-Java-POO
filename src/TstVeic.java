8621a9a2f9bb280303901e9c3fa66d1cf5ab4b7a
João Pedro Ribeiro Zugaib <zugaibplays@gmail.com>
2023-10-04 22:11:40 -0300

    correção

--- src/TstVeic.java
+++ src/TstVeic.java
@@ -41,7 +41,7 @@ public class TstVeic {
 
         // Criando um novo objeto Carga com dados inseridos pelo usuário
         Carga carga = new Carga(placaCarga, marcaCarga, modeloCarga, corCarga, qtdRodasCarga, velocMaxCarga, null, dataCadastroCarga, taraCarga, cargaMaxCarga);
-        carga.setMotor(carga.criarMotor(qtdPistoes, potencia)); // Criando o mo100tor carga
+        carga.setMotor(carga.criarMotor(qtdPistoes, potencia)); // Criando o motor carga
 
         // Exibindo todos os dados do veículo de Passeio
         System.out.println("\nDados do veículo de Passeio:\n");
